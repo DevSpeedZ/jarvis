c7b641870e655006ed2ed33d7f6236b90353da0b
Samuel Botelho <sambotelho07@gmail.com>
2025-03-19 11:09:11 -0300

    modo de transcrição

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ def falar(audio):
     texto_fala.say(audio) #falando
     texto_fala.runAndWait() #esperar
 
+#abrir um jogo da steam
 def steamgame(id):
     subprocess.Popen(["C:\\Program Files (x86)\\Steam\\steam.exe", f"steam://run/{id}"])
-
-
 
 #reconhecimento do comando
 def mic():
@@ -39,8 +38,9 @@ def mic():
         except Exception as e:
             falar("Repita por favor")
             print(e)
-            return "None"
+            return ""
 
+#loop principal
 while True:
     if keyboard.is_pressed('home'):
         print('escutando...')
@@ -51,6 +51,20 @@ while True:
             falar('abrindo marvel rivals')
             print('abrindo marvel rivals')
             steamgame(2767030)
-
+        
+        #modo transcrição
+        if 'transcricao' in comando:
+            falar('ativando modo de transcrição')
+            while True:
+                comando = unidecode(mic().lower())
+                keyboard.write(comando + " ")
+                if 'desativar' in comando:
+                    break
+#
+#tudo que eu falar agora quero que saia alias por favor saia da minha frente pois estou tentando passar tu ta com um chat da cristovao  
 #fazer a transcrição de voz para texto
+#quando ouvir a palavra descrição entrar em um loop
+#onde tudo q falado é digitado
+#ao apertar o end o loop é quebrado
+
 
